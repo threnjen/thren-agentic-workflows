0b2fe4d70adc2de506c1c17776c7a4f3166a3649
threnjen <threnjen@gmail.com>
2026-07-17 12:07:53 -0700

    cleanup

--- .opencode/plugins/done-notify.js
+++
@@ -1,7 +0,0 @@
-export const DoneNotify = async ({ $, directory }) => {
-  return {
-    "session.idle": async (_input, _output) => {
-      await $`osascript -e 'display notification "OpenCode is done" with title "OpenCode"'`.cwd(directory)
-    }
-  }
-}
